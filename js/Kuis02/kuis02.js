386ffa836d2c4b35494617f3bb3ab5a7a44de39b
AiDinaAgustin <aidinagustin2@gmail.com>
2024-03-05 09:50:05 +0700

    kuis 01

---
+++ js/Kuis02/kuis02.js
@@ -0,0 +1,20 @@
+function tambah(a , b){
+    return a + b;
+}
+
+function kurang(a , b){
+    return a - b;
+}
+
+function kali(a , b){
+    return a * b;
+}
+
+function bagi(a , b){
+    return a / b;
+}
+
+console.log(tambah(10, 20));
+console.log(kurang(10, 20));
+console.log(kali(10, 20));
+console.log(bagi(100, 20));
