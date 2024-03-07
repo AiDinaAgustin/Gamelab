23bd0952d9e51e7ce3ac6aa398b76e568527f25f
AiDinaAgustin <aidinagustin2@gmail.com>
2024-03-07 09:39:36 +0700

    kuis 06

---
+++ js/Kuis06/kuis06.js
@@ -0,0 +1,15 @@
+function hideEl() {
+    // Sembunyikan elemen h3
+    $("h3").hide();
+  }
+  
+  function hideClass() {
+    // Sembunyikan semua elemen dengan class "dengan-class"
+    $(".dengan-class").hide();
+  }
+  
+  function hideAttr() {
+    // Sembunyikan semua elemen dengan atribut href
+    $("a[href]").hide();
+  }
+  
