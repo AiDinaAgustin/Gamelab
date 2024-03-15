8d5820b637c313f3d2cb99c1271243c758c7255a
AiDinaAgustin <aidinagustin2@gmail.com>
2024-03-15 09:59:51 +0700

    done menampilkan juz data langsung tanpa button

--- js/AiDinaAgustin_TA_JS/juz.js
+++ js/AiDinaAgustin_TA_JS/juz.js
@@ -1,72 +1,82 @@
 $(document).ready(function () {
 
-    $("button.get").click(function () {
+    // Function to load all juz data
+    function loadJuzData(){
         $.ajax({
             url: "https://api.myquran.com/v2/quran/juz/semua",
             type: "GET",
             success: function (juzData) {
-                var htmlContent = "";
-
-                for (var i = 0; i < juzData.data.length; i++) {
-                    var nomorJuz = juzData.data[i].number;
-                    var namaJuz = juzData.data[i].name;
-                    var mulaiJuz = juzData.data[i].name_start_id;
-                    var ayatAwal = juzData.data[i].verse_start;
-
-                    // Build HTML structure with classes for styling
-                    htmlContent += "<li>";
-                    htmlContent += "  <div class='info-container'>";
-                    htmlContent += "    <span class='nama-juz'>" + namaJuz + "</span>";
-                    htmlContent += "  </div>";
-                    htmlContent += "  <div class='info'>";
-                    htmlContent += "    <span class='mulai-juz'>Mulai Di: " + mulaiJuz + "</span>";
-                    htmlContent += "    <span class='jumlah-ayat'>| ayat " + ayatAwal + " </span>";
-                    htmlContent += "  </div>";
-                    htmlContent += "  <div class='play'>";
-                    htmlContent += "    <button class='read-juz' data-juz='" + nomorJuz + "'>Baca</button>"; // Use "Baca" for Indonesian translation
-                    htmlContent += "    <button class='hide-juz' >Hide</button>"; // Use "Baca" for Indonesian translation
-                    htmlContent += "    <div class='ayat-container' style='display:none;'></div>"; // Container to display ayat, initially hidden
-                    htmlContent += "  </div>";
-                    htmlContent += "</li>";
-                }
-
-                $(".juz-list").html(htmlContent); // Update the content of the list
-                
-                // ** Play Button Click**
-                $("button.read-juz").click(function () {
-                    var button = $(this);
-                    var juzNumber = button.data("juz");
-                    var ayatContainer = button.siblings('.ayat-container');
-
-                    $.ajax({
-                        url: "https://api.myquran.com/v2/quran/ayat/juz/" + juzNumber,
-                        type: "GET",
-                        success: function (ayatData) {
-                            // Clear previous content and show container
-                            ayatContainer.empty().show();
-                            
-                            // Append ayat to container
-                            ayatData.data.forEach(function(ayat) {
-                                ayatContainer.append("<p>"+ ayat.ayah + ". " + ayat.arab + "</p>");
-                                ayatContainer.append("<span>" + ayat.text + "</span>");
-                            });
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            console.error("Error fetching Ayat information for Juz " + juzNumber + ":", textStatus, errorThrown);
-                        }
-                    });
-                });
-
-                // ** Hide Button Click**
-                $("button.hide-juz").click(function() {
-                    var ayatContainer = $(this).siblings('.ayat-container');
-                    ayatContainer.empty().hide();
-                    });
+                displayJuzList(juzData.data);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.error("Error fetching Juz information:", textStatus, errorThrown);
             }
         });
+    }
+
+    // Function to display the list of juz
+    function displayJuzList(DataJuz) {
+        var htmlContent = "";
+
+        DataJuz.forEach(function(juzData) {
+            var nomorJuz = juzData.number;
+            var namaJuz = juzData.name;
+            var mulaiJuz = juzData.name_start_id;
+            var ayatAwal = juzData.verse_start;
+
+                    // Build HTML structure with classes for styling
+            htmlContent += "<li>";
+            htmlContent += "  <div class='info-container'>";
+            htmlContent += "    <span class='nama-juz'>" + namaJuz + "</span>";
+            htmlContent += "  </div>";
+            htmlContent += "  <div class='info'>";
+            htmlContent += "    <span class='mulai-juz'>Mulai Di: " + mulaiJuz + "</span>";
+            htmlContent += "    <span class='jumlah-ayat'>| ayat " + ayatAwal + " </span>";
+            htmlContent += "  </div>";
+            htmlContent += "  <div class='play'>";
+            htmlContent += "    <button class='read-juz' data-juz='" + nomorJuz + "'>Baca</button>"; // Use "Baca" for Indonesian translation
+            htmlContent += "    <button class='hide-juz' >Hide</button>"; // Use "Baca" for Indonesian translation
+            htmlContent += "    <div class='ayat-container' style='display:none;'></div>"; // Container to display ayat, initially hidden
+            htmlContent += "  </div>";
+            htmlContent += "</li>";
+        });
+        $(".juz-list").html(htmlContent); 
+    }
+
+    // load all juz
+    loadJuzData();
+
+    // play button
+    $(document).on("click", "button.read-juz", function(){
+        var button = $(this);
+        var juzNumber = button.data("juz");
+        var ayatContainer = button.siblings('.ayat-container');
+
+        $.ajax({
+                url: "https://api.myquran.com/v2/quran/ayat/juz/" + juzNumber,
+                type: "GET",
+                success: function (ayatData) {
+                // Clear previous content and show container
+                ayatContainer.empty().show();
+                            
+                            // Append ayat to container
+                    ayatData.data.forEach(function(ayat) {
+                        ayatContainer.append("<p>"+ ayat.ayah + ". " + ayat.arab + "</p>");
+                        ayatContainer.append("<span>" + ayat.text + "</span>");
+                    });
+                },
+            error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error fetching Ayat information for Juz " + juzNumber + ":", textStatus, errorThrown);
+            }
+        });
+    });
+
+    // Handle Hide Button Click
+    $(document).on("click", ".hide-juz", function () {
+        var button = $(this);
+        var ayatContainer = button.siblings('.ayat-container');
+
+        ayatContainer.empty().hide();
     });
 
     // **Search**
@@ -89,4 +99,6 @@ $(document).ready(function () {
         });
     });
 
+
+   
 });
--- js/AiDinaAgustin_TA_JS/surah.js
+++ js/AiDinaAgustin_TA_JS/surah.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
-    $("button").click(function () {
-      $.ajax({
-        url: "https://api.myquran.com/v2/quran/surat/semua",
-        type: "GET",
-        success: function (res) {
-          var htmlContent = "";
-  
-          for (var i = 0; i < res.data.length; i++) {
-            var nomorSurat = res.data[i].number;
-            var namaSurat = res.data[i].name_id;
-            var ayat = res.data[i].number_of_verses;
-            var audioUrl = res.data[i].audio_url;
-            var revelation_id = res.data[i].revelation_id;
-            var translation_id = res.data[i].translation_id;
-            var tafsir = res.data[i].tafsir;
-  
+    // Function to load all surah data
+    function loadSurahData() {
+        $.ajax({
+            url: "https://api.myquran.com/v2/quran/surat/semua",
+            type: "GET",
+            success: function (res) {
+                displaySurahList(res.data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Error fetching Surah information:", textStatus, errorThrown);
+            }
+        });
+    }
+
+    // Function to display the list of surahs
+    function displaySurahList(surahs) {
+        var htmlContent = "";
+
+        surahs.forEach(function (surah) {
+            var nomorSurat = surah.number;
+            var namaSurat = surah.name_id;
+            var ayat = surah.number_of_verses;
+            var audioUrl = surah.audio_url;
+            var revelation_id = surah.revelation_id;
+            var translation_id = surah.translation_id;
+            var tafsir = surah.tafsir;
+
             // Build HTML structure with classes for styling
             htmlContent += "<li>";
             htmlContent += "  <div class='nomor'>";
@@ -29,54 +40,67 @@ $(document).ready(function () {
             htmlContent += "    <span class='jumlah-ayat'>| " + ayat + " ayat </span>";
             htmlContent += "  </div>";
             htmlContent += "  <div class='play'>";
-            htmlContent += "    <button class='read-surat' data-surat='" + nomorSurat + "'>Baca</button>"; // Use "Baca" for Indonesian translation
-            htmlContent += "    <button class='hide-surat' >Hide</button>"; // Use "Baca" for Indonesian translation
-            htmlContent += "    <div class='ayat-container' style='display:none;'></div>"; // Container to display ayat, initially hidden
+            htmlContent += "    <button class='read-surat' data-surat='" + nomorSurat + "'>Baca</button>";
+            htmlContent += "    <button class='hide-surat'>Hide</button>";
+            htmlContent += "    <div class='ayat-container' style='display:none;'></div>";
             htmlContent += "  </div>";
             htmlContent += "  <audio controls src='" + audioUrl + "'></audio>";
-            htmlContent += "  <div class='additional-info'>";  
+            htmlContent += "  <div class='additional-info'>";
             htmlContent += "    <span class='tafsir'>" + tafsir + "</span>";
-            htmlContent += "  </div>";  htmlContent += "</li>";
-          }
-  
-          $(".surat-list").html(htmlContent); // Update the content of the list
+            htmlContent += "  </div>";
+            htmlContent += "</li>";
+        });
 
-          // play button
-          $(".read-surat").click(function() {
-            var button = $(this);
-            var suratNumber = button.data("surat");
-            var ayatContainer = button.siblings('.ayat-container');
+        $(".surat-list").html(htmlContent); // Update the content of the list
+    }
 
-            $.ajax({
-                url: "https://quran-api.santrikoding.com/api/surah/" + suratNumber,
-                type: "GET",
-                success: function(suratData) {
-                    ayatContainer.empty().show();
+    // Load all surah data initially
+    loadSurahData();
 
-                    suratData.ayat.forEach(function(ayat){
-                        ayatContainer.append("<p>"+ ayat.id + ". " + ayat.ar + "</p>");
-                        ayatContainer.append("<span>" + ayat.idn + "</span>");
-                    });
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    console.error("Error fetching Ayat information for Surat " + suratNumber + ":", textStatus, errorThrown);
-                }
-            });
-          });
+    // Handle Play Button Click
+    $(document).on("click", ".read-surat", function () {
+        var button = $(this);
+        var suratNumber = button.data("surat");
+        var ayatContainer = button.siblings('.ayat-container');
 
-          // Button hide
-          $(".hide-surat").click(function() {
-            var button = $(this);
-            var ayatContainer = button.siblings('.ayat-container');
+        $.ajax({
+            url: "https://quran-api.santrikoding.com/api/surah/" + suratNumber,
+            type: "GET",
+            success: function (suratData) {
+                ayatContainer.empty().show();
 
-            ayatContainer.empty().hide();
-          });
-        },
-
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error("Error fetching Juz information:", textStatus, errorThrown);
-        }
-      });
+                suratData.ayat.forEach(function (ayat) {
+                    ayatContainer.append("<p>" + ayat.nomor + ". " + ayat.ar + "</p>");
+                    ayatContainer.append("<span>" + ayat.idn + "</span>");
+                });
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Error fetching Ayat information for Surat " + suratNumber + ":", textStatus, errorThrown);
+            }
+        });
     });
-  });
-  
+
+    // Handle Hide Button Click
+    $(document).on("click", ".hide-surat", function () {
+        var button = $(this);
+        var ayatContainer = button.siblings('.ayat-container');
+
+        ayatContainer.empty().hide();
+    });
+
+    // Handle Search
+    $("#search-form").submit(function (event) {
+        event.preventDefault(); // Prevent form submission
+
+        var searchText = $("#search-input").val().toLowerCase();
+        $(".nama-surat").each(function () {
+            var suratName = $(this).text().toLowerCase();
+            var parentLi = $(this).closest("li");
+            if (suratName.indexOf(searchText) !== -1 || searchText === "") {
+                parentLi.show();
+            } else {
+                parentLi.hide();
+            }
+        });
+    });
+});
